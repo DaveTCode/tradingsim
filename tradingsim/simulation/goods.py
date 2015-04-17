1b17fc295223ca3dac10e1ae5e87993815b69728
David Tyler <davet.code@gmail.com>
2015-04-17 19:01:15 +0100

    Added commenting for each of the functions

--- tradingsim/simulation/goods.py
+++ tradingsim/simulation/goods.py
@@ -2,6 +2,10 @@ import math
 
 
 class Goods:
+    """
+    This represents the static data that constitutes a single good. It
+    defines the cost of that good.
+    """
 
     def __init__(self, name, max_cost, min_cost, min_cost_amount):
         self.name = name
@@ -10,9 +14,20 @@ class Goods:
         self.min_cost_amount = float(min_cost_amount)
 
     def cost_difference(self):
+        """
+        :return: The difference in cost between the minimum and maximum for this good.
+        """
         return self.min_cost - self.max_cost
 
     def purchase_cost_of_one(self, current_amount):
+        """
+        Used to calculate the cost of purchasing a single instance of this good
+        given that the owner currently has "current_amount"
+
+        :param current_amount: The amount in stock at the owner
+
+        :return: The cost of a single instance of this good.
+        """
         current_amount = float(current_amount)
         if current_amount > self.min_cost_amount:
             return self.min_cost
@@ -21,6 +36,16 @@ class Goods:
             return -1.0 * m * current_amount + self.max_cost
 
     def purchase_cost(self, current_amount, purchase_amount):
+        """
+        Used to calculate the cost to purchase x of the good when the owner has
+        y of them.
+
+        :param current_amount: The amount that the owner currently has in
+                               stock.
+        :param purchase_amount: The amount that the buyer wants.
+
+        :return: The total cost to purchase the required number.
+        """
         initial_cost = self.purchase_cost_of_one(current_amount)
         final_cost = self.purchase_cost_of_one(current_amount - purchase_amount)
         total_cost = math.ceil(float(purchase_amount) * (initial_cost + final_cost) / 2.0)
@@ -29,8 +54,8 @@ class Goods:
 
     def sale_cost(self, current_amount, sale_amount):
         """
-            TODO: For now the sale cost is defined as equal to the purchase
-            cost.
+        TODO: For now the sale cost is defined as equal to the purchase
+        cost.
         """
         initial_cost = self.purchase_cost_of_one(current_amount)
         final_cost = self.purchase_cost_of_one(current_amount + sale_amount)
